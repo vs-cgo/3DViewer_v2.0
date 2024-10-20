ccd77e05adaed52de75d483fbe513c9b38b6ca88
vs-cgo <victorshavlikov@gmail.com>
2024-10-20 15:12:50 +0300

    Delete src directory

--- src/view/memento/memento.cc
+++
@@ -1,8 +0,0 @@
-#include "./memento.h"
-
-namespace s21 {
-Memento::Memento(Data &data) : state_(data){};
-void Memento::SetState(Data const &data) { state_ = data; }
-Data Memento::GetState() { return state_; };
-
-}  // end namespace s21
